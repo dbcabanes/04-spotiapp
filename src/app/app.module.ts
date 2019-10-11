b1bbcaea4ee1a8f7dffd12bca3af73afeac50d71
dbcabanes <dborrascabanes@hotmail.com>
2019-10-11 10:54:13 +0200

    no message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { SearchComponent } from './components/search/search.component';
-import { ArtistaComponent } from './components/artista/artista.component';
-import { NavbarComponent } from './components/Shared/navbar/navbar.component';
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./components/home/home.component";
+import { SearchComponent } from "./components/search/search.component";
+import { ArtistaComponent } from "./components/artista/artista.component";
+import { NavbarComponent } from "./components/Shared/navbar/navbar.component";
+// importar Rutas
+import { RouterModule } from "@angular/router";
+import { ROUTES } from "./app.routes";
 
 @NgModule({
   declarations: [
@@ -15,10 +18,8 @@ import { NavbarComponent } from './components/Shared/navbar/navbar.component';
     ArtistaComponent,
     NavbarComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(ROUTES, { useHash: true })],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
